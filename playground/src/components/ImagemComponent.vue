f0af6ded80c3b344147c999d5b632d3dfd21aa37
Davi  Campos Parente <04742768132@dccbra.ifb.local>
2024-01-16 20:35:25 -0300

    Aula 16-01

--- playground/src/components/ImagemComponent.vue
+++ playground/src/components/ImagemComponent.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <p>Nome da Imagem: {{nomeImagem}}</p>
         <img v-show="imagemVisivel" :src="urlImg" alt="imagem banner">
         <button @click="aparece_esconde(), segundoMetodo()">Aparece/Esconde</button>
     </div>
@@ -52,8 +53,10 @@
             }
         },
         props:{
-            urlImg: String
+            urlImg: String,
+            nomeImagem: String
         },
+        emits:["visibilidadeImg"],
         methods: {
             aparece_esconde(){
                 this.imagemVisivel = !this.imagemVisivel;
@@ -63,6 +66,7 @@
                 setTimeout(function(){
                     console.log("Segundo método chamado juntamente do click");
                 }, 2000);
+                this.$emit('visibilidadeImg', this.imagemVisivel);
             }
         }
     }
